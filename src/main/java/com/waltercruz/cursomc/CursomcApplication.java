7eea91f66541c0bee674cca0e495c72d8f88f48b
Walter Cruz <fulano@uolinc.com>
2018-10-11 16:39:33 -0300

    Criado operacao de instanciacao

--- src/main/java/com/waltercruz/cursomc/CursomcApplication.java
+++ src/main/java/com/waltercruz/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.waltercruz.cursomc;
 
+import com.waltercruz.cursomc.domain.Categoria;
+import com.waltercruz.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
+
+/* CommandLineRunner Executar a instanciação no momento que a app iniciar*/
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
     }
 }
