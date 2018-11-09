2c0aa1cf2cd61afe94429e1dd4e4aa9e2751adc9
Walter Cruz <fulano@uolinc.com>
2018-11-09 17:35:52 -0200

    Cidade + Estado

---
+++ src/main/java/com/waltercruz/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,20 @@
+package com.waltercruz.cursomc.repositories;
+
+
+
+import com.waltercruz.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+
+
+
+
+}
+
+
+
+
