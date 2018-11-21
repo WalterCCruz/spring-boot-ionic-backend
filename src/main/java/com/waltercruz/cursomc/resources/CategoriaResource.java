a60fca385c4e8beeda224a91f122165d8b694d2f
Walter Cruz <fulano@uolinc.com>
2018-11-21 14:00:27 -0200

    Insert de categoria method POST

--- src/main/java/com/waltercruz/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/waltercruz/cursomc/resources/CategoriaResource.java
@@ -4,10 +4,10 @@ import com.waltercruz.cursomc.services.CategoriaService;
 import com.waltercruz.cursomc.domain.Categoria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 
 /* O RestController serializa o objeto para que a resposta seja exibida, tornando disponsável o uso do @ResponseBody no método que irá retornar*/
@@ -26,6 +26,14 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    /*@RequestBody esta annotation permite que vc transforme seu json automaticamente para seu objeto de domain (entidade)*/
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = categoriaService.insert(obj);
+        /*Neste trechoestou associando para URL o id criado para o novo objeto e já atribuindo o seu direcionamento*/
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 
 
 
--- src/main/java/com/waltercruz/cursomc/services/CategoriaService.java
+++ src/main/java/com/waltercruz/cursomc/services/CategoriaService.java
@@ -21,4 +21,11 @@ public class CategoriaService {
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
+
+
+    public Categoria insert (Categoria obj){
+        obj.setId(null);/*para garantir que o objeto que estou inserindo é novo*/
+        return categoriaRepository.save(obj);
+    }
+
 }
