d68cfc7f26af63f89d2ce88407a15457075ffaf3
Walter Cruz <fulano@uolinc.com>
2018-11-23 14:19:30 -0200

    Aula 38 8:11

--- src/main/java/com/waltercruz/cursomc/resources/ClienteResource.java
+++ src/main/java/com/waltercruz/cursomc/resources/ClienteResource.java
@@ -1,15 +1,17 @@
 package com.waltercruz.cursomc.resources;
 
-import com.waltercruz.cursomc.domain.Categoria;
+import com.waltercruz.cursomc.DTO.ClienteDTO;
 import com.waltercruz.cursomc.domain.Cliente;
-import com.waltercruz.cursomc.services.CategoriaService;
+import com.waltercruz.cursomc.domain.Cliente;
+import com.waltercruz.cursomc.services.ClienteService;
 import com.waltercruz.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /* O RestController serializa o objeto para que a resposta seja exibida, tornando disponsável o uso do @ResponseBody no método que irá retornar*/
@@ -21,13 +23,48 @@ public class ClienteResource {
     private ClienteService clienteService;
 
     /*PathVariable é a variável que virá da URL conforme o parametro solciitado acima no RequestMapping */
-    /*Response Entity já encapusla o meu objeto, dispensando a necessidade de constuir uma classe de ResponseCategoria*/
+    /*Response Entity já encapusla o meu objeto, dispensando a necessidade de constuir uma classe de ResponseCliente*/
     @RequestMapping(value= "/{id}",method = RequestMethod.GET)
     public ResponseEntity<Cliente> find(@PathVariable Integer id) {
         Cliente obj = clienteService.find(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody Cliente obj, @PathVariable Integer id) {
+        obj.setId(id);
+        obj = clienteService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ClienteDTO>> findAll() {
+        List<Cliente> list = clienteService.findAll();
+        /*Esta utilizsacao do DTO serve para que nao traga informacoes desnecessarias atraladas a categoria para este metodo ex: produtos
+         * Com a utilizacao do lambda nao necessito realizar o for no DTO/Response para realizar a varredura e conversao das listas de Cliente
+         * para CateGoriaDTO*/
+        List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    /*@RequestParam faz com que o parametro se torne opcional*/
+    public ResponseEntity<Page<ClienteDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+        Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
+        Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+        return ResponseEntity.ok().body(listDTO);
+    }
 
 
 
--- src/main/java/com/waltercruz/cursomc/services/ClienteService.java
+++ src/main/java/com/waltercruz/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.waltercruz.cursomc.services;
 
 
+import com.waltercruz.cursomc.DTO.ClienteDTO;
+import com.waltercruz.cursomc.domain.Cliente;
 import com.waltercruz.cursomc.domain.Cliente;
 import com.waltercruz.cursomc.repositories.ClienteRepository;
+import com.waltercruz.cursomc.services.exception.DataIntegrityException;
 import com.waltercruz.cursomc.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,4 +29,48 @@ public class ClienteService {
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
     }
+
+
+    public Cliente insert (Cliente obj){
+        obj.setId(null);/*para garantir que o objeto que estou inserindo é novo*/
+        return clienteRepository.save(obj);
+    }
+
+    /*Cliente valendo null insere se já estiver preenchido id é realizada a edicao*/
+    public Cliente update (Cliente obj){
+        find(obj.getId());
+        return clienteRepository.save(obj);
+
+    }
+
+
+    public void delete (Integer id){
+        find(id);
+        try {
+            clienteRepository.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir uma cliente que possui produtos.");
+        }
+
+    }
+
+
+    public List<Cliente> findAll(){
+        return clienteRepository.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPePage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page,linesPePage, Sort.Direction.valueOf(direction),orderBy);
+        return clienteRepository.findAll(pageRequest);
+    }
+
+
+    public Cliente fromDTO (ClienteDTO objetoDto){
+        throw new UnsupportedOperationException();
+    }
+
+
+
+
+
 }
