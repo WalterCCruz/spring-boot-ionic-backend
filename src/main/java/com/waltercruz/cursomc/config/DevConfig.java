e219ba686ef9e40c0919accc35c33a259afeb9cf
Walter Cruz <fulano@uolinc.com>
2018-12-10 16:39:51 -0200

    criando profile desenvolvimento

---
+++ src/main/java/com/waltercruz/cursomc/config/DevConfig.java
@@ -0,0 +1,37 @@
+package com.waltercruz.cursomc.config;
+
+
+import com.waltercruz.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("$spring.jpa.hibernate.ddl-auto")
+    private String strategy;
+
+    @Bean
+    public Boolean instantiateDatabase() throws ParseException {
+
+        if(!"create".equals(strategy)) {
+            dbService.instantiateDatabase();
+            return true;
+        }else {
+            return true;
+        }
+
+
+    }
+
+
+}
