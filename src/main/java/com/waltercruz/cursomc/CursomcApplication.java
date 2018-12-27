ad39ac17a49fd9bad00d24b90f0d86a2140c2c88
Walter Cruz <fulano@uolinc.com>
2018-12-27 18:04:32 -0200

    salvando arquivos S3 amazon

--- src/main/java/com/waltercruz/cursomc/CursomcApplication.java
+++ src/main/java/com/waltercruz/cursomc/CursomcApplication.java
@@ -4,6 +4,7 @@ import com.waltercruz.cursomc.domain.*;
 import com.waltercruz.cursomc.domain.Enums.EstadoPagamento;
 import com.waltercruz.cursomc.domain.Enums.TipoCliente;
 import com.waltercruz.cursomc.repositories.*;
+import com.waltercruz.cursomc.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,12 +18,15 @@ public class CursomcApplication implements CommandLineRunner {
 
 
 
+    @Autowired
+    private S3Service s3Service;
+
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        s3Service.uploadFile("C:\\java.jpg");
     }
 }
