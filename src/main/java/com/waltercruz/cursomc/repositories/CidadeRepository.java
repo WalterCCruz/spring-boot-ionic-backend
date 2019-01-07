e2b9285e834eed15700cedee7e448d7b1ae9f214
Walter Cruz <fulano@uolinc.com>
2019-01-07 18:43:17 -0200

    padronizando formato de excecoes

--- src/main/java/com/waltercruz/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/waltercruz/cursomc/repositories/CidadeRepository.java
@@ -13,11 +13,12 @@ import java.util.List;
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
 
-    @Transactional(readOnly = true)
+    @Transactional(readOnly=true)
     @Query("SELECT obj FROM Cidade obj WHERE obj.estado.id = :estadoId ORDER BY obj.nome")
-    public List<Cidade> findCidades(@Param("estado_id") Integer estado_id);
-
+    public List<Cidade> findCidades(@Param("estadoId") Integer estado_id);
 }
+
+
 
 
 
--- src/main/java/com/waltercruz/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/waltercruz/cursomc/resources/exception/StandardError.java
@@ -3,30 +3,25 @@ package com.waltercruz.cursomc.resources.exception;
 
 import java.io.Serializable;
 
-public class StandardError  implements Serializable {
-
+public class StandardError implements Serializable {
     private static final long serialVersionUID = 1L;
 
-
-    private Integer status;
-    private String msg;
     private Long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
 
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
+    public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+        super();
+        this.timestamp = timestamp;
         this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
     }
 
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
+    public StandardError(Integer status, String msg, Long timestamp) {
     }
 
     public Long getTimestamp() {
@@ -37,9 +32,36 @@ public class StandardError  implements Serializable {
         this.timestamp = timestamp;
     }
 
-    public StandardError(Integer status, String msg, Long timestamp) {
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
         this.status = status;
-        this.msg = msg;
-        this.timestamp = timestamp;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 }
+
